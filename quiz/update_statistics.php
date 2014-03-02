f14491a7b1d9fb804cb3a85967d9c86ce3fec06c
Roger Walt <roger.walt@gmail.com>
2014-03-02 18:20:23 +0100

    updating statistics correctly

--- quiz/update_statistics.php
+++ quiz/update_statistics.php
@@ -1,3 +1,18 @@
 <?
-var_dump($_GET['id']);
-var_dump($_GET['correct']);
+$dbh = new PDO('mysql:host=localhost;dbname=segelflug;charset=utf8', 'segelflug', 'AyurBGRjW9cz2qx3');
+
+switch ($_GET['correct']) {
+	case true:
+		$sql = 'UPDATE answer_statistics SET correct=correct+1 WHERE id_question = '.intval($_GET['id']).';';
+		break;
+	case false:
+		$sql = 'UPDATE answer_statistics SET wrong=wrong+1 WHERE id_question = '.intval($_GET['id']).';';
+		break;
+	default:
+		throw new Exception('Data malformatted.');
+		break;
+}
+
+$dbh->exec($sql);
+
+echo json_encode(array('success' => true));
